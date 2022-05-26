2301b90d99d3734be3321042651beffc797bb6ac
SantiCampoamor <101577703+SantiCampoamor@users.noreply.github.com>
2022-05-26 09:07:55 -0300

    Gallina created

---
+++ Assets/PlayerBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject spawnPoint;
+   
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject clon;
+            clon = Instantiate(prefab);
+            clon.transform.position = spawnPoint.transform.position; 
+        }
+    }
+}
